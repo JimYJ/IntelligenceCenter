6327ee291116e9e47a8f526acfb30345af7c76e6
JimYJ <yblues@hotmail.com>
2024-10-08 22:48:22 +0800

    修改结构

--- router/api.go
+++ router/api.go
@@ -1,1 +1,20 @@
 package router
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Api() {
+	r := gin.Default()
+	r.GET("/api/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "hello world",
+		})
+	})
+	err := r.Run(":6061")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
--- router/web.go
+++ router/web.go
@@ -28,16 +28,3 @@ func Web(static embed.FS) {
 		fmt.Println(err)
 	}
 }
-
-func Api() {
-	r := gin.Default()
-	r.GET("/api/test",func(c *gin.Context) {
-		c.JSON(200, gin.H{
-		  "msg": "hello world",
-		})
-	})
-	err := r.Run(":6061")
-	if err != nil {
-		fmt.Println(err)
-	}
-}
